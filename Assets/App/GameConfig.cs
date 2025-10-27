af12573563c6633f0ed32b6f733c8831d5af0a5a
ChangDae1993 <jsj939639@gmail.com>
2025-10-27 22:20:58 +0900

    ADD : Basic Form
    TODO :
    PlayerInput 제작 시작하기

---
+++ Assets/App/GameConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Heist/GameConfig")]
+
+public class GameConfig : MonoBehaviour
+{
+    [Header("Speed")]
+    public float forwardSpeed = 15f;     // basic move forward speed
+    public float lateralSpeed = 8f;     // basic move side speed
+    public float laneLimit = 5f;        // limit move side
+
+    [Header("Road")]
+    public float chunkLength = 30f;     // chunk length;
+    public int initialChunks = 6;       // initial chunk count;
+}
---
+++ Assets/Game/Player/CarController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private GameConfig config;
+
+    private Rigidbody rb;
+    private float moveInput;    // -1~1
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if(config == null )
+        {
+            Debug.LogWarning("CarController : GameConfig not assigned.");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float forward = (config ? config.forwardSpeed : 15f) * Time.fixedDeltaTime;
+        float leteral = (config ? config.forwardSpeed : 8f) *moveInput * Time.fixedDeltaTime;
+
+        Vector3 next = rb.position + Vector3.forward * forward + Vector3.right * leteral;
+
+        // move side limit
+        float limit = config ? config.laneLimit : 5f;
+        next.x = Mathf.Clamp(next.x, -limit, limit);
+
+        rb.MovePosition(next);
+    }
+
+    public void SetMoveInput(float input01)
+    {
+        moveInput = Mathf.Clamp(input01, -1f, 1f);
+    }
+
+}
