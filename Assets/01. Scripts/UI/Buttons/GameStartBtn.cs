4aaf7c67c82a11a3cb29511d1e97c01ad29367aa
ChangDae1993 <jsj939639@gmail.com>
2025-12-21 17:22:38 +0900

    GameStart버튼 추가

---
+++ Assets/01. Scripts/UI/Buttons/GameStartBtn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartBtn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void LoadSceneName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
